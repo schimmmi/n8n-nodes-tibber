b233077b8cf15abae9b6529112714b9e04a43b75
Juergen Schilling <juergen_schilling@web.de>
2025-08-11 18:21:34 +0200

    Add `clean` task to Gulp; update `build` task to include cleaning dist folder; fix paths for nodes and credentials

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { task, src, dest, series } = require('gulp');
+const clean = require('gulp-clean');
 
+task('clean', cleanDist);
 task('build:icons', copyIcons);
+task('build', series('clean', 'build:icons'));
+
+function cleanDist() {
+	return src('dist', { read: false, allowEmpty: true })
+		.pipe(clean());
+}
 
 function copyIcons() {
-	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
-	const nodeDestination = path.resolve('dist', 'nodes');
+	const nodeSource = path.resolve('src', 'nodes', '**', '*.{png,svg}');
+	const nodeDestination = path.resolve('dist', 'src', 'nodes');
 
 	src(nodeSource).pipe(dest(nodeDestination));
 
-	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
-	const credDestination = path.resolve('dist', 'credentials');
+	const credSource = path.resolve('src', 'credentials', '**', '*.{png,svg}');
+	const credDestination = path.resolve('dist', 'src', 'credentials');
 
 	return src(credSource).pipe(dest(credDestination));
 }
